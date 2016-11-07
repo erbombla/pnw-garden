7a443d7d76a87f9b7f4b3bcb28d8d209542f3a4b
Frankie Gavin <frankanthonygavin@gmail.com>
2016-11-07 15:13:34 -0800

    add placeholder images, change to a grid layout, and clickable seasons to diplay respective vegetables.

--- js/script.js
+++ js/script.js
@@ -13,3 +13,28 @@
 
 
 //FRONT-END LOGIC
+$(document).ready(function(){
+  $("#spring-click").click(function() {
+    $("#spring").show();
+  $("#spring-click").click(function() {
+    $("#spring").show();
+  $("#spring-click").click(function() {
+    $("#spring").show();
+  $("#spring-click").click(function() {
+    $("#spring").show();
+});
+});
+});
+});
+
+
+
+
+
+
+
+
+
+
+
+});
