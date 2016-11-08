a54467f3ab5175d3054a9b2e81af37c7d3562968
Austin Blain and Ellie Angell <ellie@>
2016-11-08 09:32:48 -0800

    turn to radio buttons

--- js/script.js
+++ js/script.js
@@ -1,31 +1,34 @@
 debugger;
-var When = function(season) {
+var Gwhendalyn = function(season) {
   this.season = season
 }
 
-var filledOut = new When([])
+var filledOut = new Gwhendalyn([])
 
-When.prototype.gardenTime = function() {
+Gwhendalyn.prototype.gardenTime = function() {
+  debugger;
   if (this.season.includes("55-65 degrees")) {
     this.season = "Winter";
-  } else if (this.season.includes("55-65 degrees")) {
+  } else if (this.season.includes("")) {
     this.season = "Spring";
-  } else if (this.season.includes("55-65 degrees")) {
+  } else if (this.season.includes("frar")) {
     this.season = "Fall";
-  } else if (this.season.includes("55-65 degrees")) {
+  } else if (this.season.includes("ryry")) {
     this.season = "Summer";
+  } else {
+    alert("Fill Out the survey!");
   }
 }
 
 
-
 $(document).ready(function() {
-  event.preventDefault();
-
+  debugger;
   var temperature = $("#temperature").val();
   var space = $("#space").val();
   var eating = $("#eating").val();
   var time = $("#time").val();
 
-
+  $("#survey").submit(function(event) {
+    event.preventDefault();
+  });
 });
