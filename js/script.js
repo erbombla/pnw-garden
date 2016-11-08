75679e38c4ec8f6532a4981c8f54af4df2f7f00a
Austin Blain and Ellie Angell <ellie@>
2016-11-08 11:35:15 -0800

    run the survey

--- js/script.js
+++ js/script.js
@@ -1,34 +1,70 @@
-debugger;
-var Gwhendalyn = function(season) {
-  this.season = season
+var answer;
+
+var Season = function(as, bs, cs){
+  this.aCount = as
+  this.bCount = bs
+  this.cCount = cs
 }
 
-var filledOut = new Gwhendalyn([])
+var theCounts = new Season([], [], [])
 
-Gwhendalyn.prototype.gardenTime = function() {
+function sortIt(i){
   debugger;
-  if (this.season.includes("55-65 degrees")) {
-    this.season = "Winter";
-  } else if (this.season.includes("")) {
-    this.season = "Spring";
-  } else if (this.season.includes("frar")) {
-    this.season = "Fall";
-  } else if (this.season.includes("ryry")) {
-    this.season = "Summer";
-  } else {
-    alert("Fill Out the survey!");
+  if (i === "a") {
+    theCounts.aCount.push(i)
+  } else if (i === "b") {
+    theCounts.bCount.push(i)
+  } else if (i === "c") {
+    theCounts.cCount.push(i)
   }
 }
 
+Season.prototype.makeAChoice = function(){
+  debugger;
+  if (this.aCount.length === 2) {
+    answer = "Spring";
+    return answer;
+  } else if (this.bCount.length === 2) {
+    answer = "Summer";
+    return answer;
+  } else if (this.cCount.length === 2) {
+    answer = "Fall";
+    return answer;
+  } else {
+    answer = "Everything"
+  }
+}
 
+// User End Logic! ------------->
 $(document).ready(function() {
   debugger;
-  var temperature = $("#temperature").val();
-  var space = $("#space").val();
-  var eating = $("#eating").val();
-  var time = $("#time").val();
+
 
   $("#survey").submit(function(event) {
+    debugger;
     event.preventDefault();
+    var temperature = $("input:radio[name=temp]:checked").val();
+    sortIt(temperature);
+    var eating = $("input:radio[name=eat]:checked").val();
+    sortIt(eating);
+    var time = $("input:radio[name=time]:checked").val();
+    sortIt(time);
+
+    theCounts.makeAChoice();
+
+    if (answer === "Spring") {
+      $("#answerHere").text("You will enjoy gardening in the Spring the most!")
+    } else if (answer === "Summer") {
+      $("#answerHere").text("You will enjoy gardening in the Summer the most!")
+    } else if (answer === "Fall") {
+      $("#answerHere").text("You will enjoy gardening in the Fall the most!")
+    } else if (answer === "Everything") {
+      $("#answerHere").text("You will enjoy gardening any time of the year!")
+    }
+
+    theCounts.aCount = [];
+    theCounts.bCount = [];
+    theCounts.cCount = [];
   });
+
 });
